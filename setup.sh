b6798e07238f037bf9d7992ed0be10bfd225b18b
Michael Crilly <mike@opsfactory.com.au>
2023-03-02 08:47:44 +1000

    attempting to solve issue with data zip

--- setup.sh
+++ setup.sh
@@ -54,7 +54,8 @@ mkdir -p "${HOME}/${AZEROTHCORE_SERVER_DIR}/bin/data"
 # We download the v16 maps, mmaps, VMOs, cameras, etc.
 if [ ! -f "${HOME}/${AZEROTHCORE_SERVER_DIR}/bin/data/v16.zip" ];
 then
-  wget https://github.com/wowgaming/client-data/releases/download/v16/data.zip "${HOME}/${AZEROTHCORE_SERVER_DIR}/bin/data/v16.zip"
+  wget https://github.com/wowgaming/client-data/releases/download/v16/data.zip
+  mv data.zip "${HOME}/${AZEROTHCORE_SERVER_DIR}/bin/data/v16.zip"
   # Extract them to the server's data directory
   unzip "${HOME}/${AZEROTHCORE_SERVER_DIR}/bin/data/v16.zip" -d "${HOME}/${AZEROTHCORE_SERVER_DIR}/bin/data"
 fi
